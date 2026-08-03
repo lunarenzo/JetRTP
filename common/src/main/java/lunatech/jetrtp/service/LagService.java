2fe43d3be1a21415c7a60dc731796882119c049b
lunarenzo <lunarenzo@users.noreply.github.com>
2026-08-04 02:15:42 +0800

    doc: fix javadoc inequality warning in LagService

--- common/src/main/java/lunatech/jetrtp/service/LagService.java
+++ common/src/main/java/lunatech/jetrtp/service/LagService.java
@@ -2,7 +2,7 @@ package lunatech.jetrtp.service;
 
 public interface LagService {
     /**
-     * Checks if the server is currently lagging (TPS < 19.0 or MSPT > 50.0).
+     * Checks if the server is currently lagging ({@code TPS < 19.0 or MSPT > 50.0}).
      *
      * @return true if the server is lagging, false otherwise
      */
